b7afb10b1fb81a96fb240f1c4e4a17b5951b5d82
upgrace-in <thedesiretree47@gmail.com>
2022-12-04 18:17:51 +0530

    work

--- TransactionScript/TransactionScript.py
+++ TransactionScript/TransactionScript.py
@@ -6,10 +6,14 @@ from time import sleep
 
 '''
 Instructions to run:
+Change your wallet address and PRIVKEY
+
 ###pip install web3
 
 Run on Terminal:
 ###python TransactionScript.py
+
+Check that you haev enpough gas fee for the transactions + SJT tokens
 '''
 
 # Switch Mainnet/Testnet
@@ -17,10 +21,11 @@ CHAINID = 5  # 1 for Ethereum mainnet & 5 for Goerli testnet
 TESTURL = "https://eth-goerli.g.alchemy.com/v2/surwT5Ql_QhEc083ru_C98XrwbDj-jVx"
 
 # Wallet Address Details
-PRIVKEY = "554dbeb6cefa25e3694ff33781cae24291443f31b6f861ecef228a007c0da323" # Private key of your wallet
-WALLETADDRESS = "0x2caF424F1BcbEf1f1D7dF082c6b5677f0283f9d7" # Address of your wallet
+PRIVKEY = "4349749f97226605564c20fa6b9f35f259456a710ce23ca01bffe239cab3ae5f" # Private key of your wallet
+WALLETADDRESS = "0x04c63D8c2fc9DD602aeE46F12083af1DdE69C713" # Address of your wallet (SJT + GAS fee should be their)
 
-WalletAddresses = ['0xe6ea3Cde2d567993E80cc44Ce0c308Da94965F3f',
+# Below where the receivers are :)
+WalletAddresses = ['0x2caF424F1BcbEf1f1D7dF082c6b5677f0283f9d7',
                    '0xC5EE6df742d1Ec2D0C7fFA93CFCb92eaE1eFE865', '0x9C96a2B3FB3871e6CE4ea6587801F89D2a6094e1', '0x990c8121ec42C9b7a3049Df585352759c2eA149e', '0x44492C089a11D4d60B8D0EB3f8eC0EFA14F88f6b']
 
 # Initializing web3
